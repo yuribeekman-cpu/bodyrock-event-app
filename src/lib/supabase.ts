e82fe43689b972eafd91c1eda1da6586593b26aa
YuriBeekman <yuri.beekman@gmail.com>
2026-06-28 22:02:56 +0200

    Anon update

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://qkhnjejjkrznbcpgmbxv.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFraG5qZWpqa3J6bmJjcGdtYnh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk4MjIzNDUsImV4cCI6MjAyNTM5ODM0NX0.Q-4BnrjH5e5Ui9J4T049FfNGoHKpF9IdxfjX1fnuCSs'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFraG5qZWpqa3J6bmJjcGdtYnh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5MTE4MjAsImV4cCI6MjA5NzQ4NzgyMH0.Q-4BnrjH5e5Ui9J4TO49FfNGoHKpF9IdxfjX1fnuCSs'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
