a064db52e2708a7eb7b0750c4b42d33a2c9e650c
YuriBeekman <yuri.beekman@gmail.com>
2026-06-28 18:55:43 +0200

    Bugfixs

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = 'force-dynamic'
-
 'use client'
+
+export const dynamic = 'force-dynamic'
 
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
